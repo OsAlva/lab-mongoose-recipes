ebe7c567bff48b5cff24d1482952f992c02986ce
Oscar Alva <oscaraldrin23@gmail.com>
2022-10-07 21:39:00 +0200

    lab hecho

--- index.js
+++ index.js
@@ -15,9 +15,52 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
+  // .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
+     //Creamos una receta
+    // const recipeOne = {
+    //   title: "Chocolate Chip Cookies2",
+    //   level: "Amateur Chef",
+    //   ingredients: [
+    //     "1/2 cup light brown sugar",
+    //     "1 large egg",
+    //     "2 tablespoons milk",
+    //     "1 1/4 teaspoon vanilla extract",
+    //     "2 cups semisweet chocolate chips"
+    //   ],
+    //   cuisine: "French",
+    //   dishType: "dessert",
+    //   image: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F4398987.jpg&w=596&h=399.32000000000005&c=sc&poi=face&q=85',
+    //   duration: 30,
+    //   creator: "Chef Jennifer",
+    // };
+
+    //   return Recipe.create(recipeOne);
+  // })
+  // .then((recipeDB) => {
+  //   console.log("Created a new recipe: ",recipeDB.title);
+  // })
+    .then(() =>{
+      return Recipe.insertMany(data);
+    })
+    .then((recipeArr) => {
+      recipeArr.forEach((recipe) => {
+        console.log(recipe.title)
+      })
+  
+      return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, { returnDocument: 'after' })
+    })
+    .then(update => {
+      console.log("successful")
+      console.log(update)
+
+      return Recipe.deleteOne({title: "Carrot Cake"});
+    })
+    .then(removeCarrot => {
+      console.log("success Remove");
+      console.log(removeCarrot)
+      mongoose.connection.close(MONGODB_URI)
+    })
+   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+    })
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,47 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//creamos esquema
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients:{
+    type: [String],
+  },
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: String,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration:{
+    type: Number,
+    min: 0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
+//creamos modelo
 const Recipe = mongoose.model('Recipe', recipeSchema);
-
+//exportamos el modelo
 module.exports = Recipe;
